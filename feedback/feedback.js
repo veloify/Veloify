b47e2250a0260db7a5f64e4f6fc2e40533209e46
veloify <veloifybr@gmail.com>
2025-06-18 00:15:53 -0300

    Add files via upload

--- feedback/feedback.js
+++ feedback/feedback.js
@@ -7,9 +7,6 @@ function fecharIframe() {
 // função para fechar o feedback
 const closeFeed = document.getElementById("closeFeedback");
 closeFeed.addEventListener("click", () => {
-  const formFeed = document.getElementById("formFeed");
-
-  formFeed.style.display = "none";
 
   fecharIframe();
 });
@@ -38,7 +35,6 @@ form.addEventListener('submit', async function (e) {
     });
 
     if (response.ok) {
-      form.style.display = 'none';
       fecharIframe();
       messageSucess();
 
